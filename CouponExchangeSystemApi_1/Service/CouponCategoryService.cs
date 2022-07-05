62bbaa7e49c51505d742fb92311c1d1e7f0fd41f
Bhavesh Kripal <kripal310bhavesh@gmail.com>
2022-07-05 17:59:46 +0530

    First Commit

---
+++ CouponExchangeSystemApi_1/Service/CouponCategoryService.cs
@@ -0,0 +1,42 @@
+﻿using CouponExchangeSystemApi_1.Data;
+using CouponExchangeSystemApi_1.Interface;
+using CouponExchangeSystemApi_1.ViewModels;
+using System.Linq;
+
+namespace CouponExchangeSystemApi_1.Service
+{
+    public class CouponCategoryService : ICouponCategoryService
+    {
+        private AppDbContext appDbContext;
+        public CouponCategoryService(AppDbContext _appDbContext)
+        {
+            appDbContext = _appDbContext;
+        }
+        public CouponCategoryData AddCouponCategory(CouponCategoryData ccd)
+        {
+            return null;
+        }
+        public CouponCategoryData UpdateCouponCategory(CouponCategoryData ccd)
+        {
+            return null;
+        }
+        public CouponCategoryData GetCouponCategoryById(int couponCategoryId)
+        {
+            return null;
+        }
+        public CouponCategoryData GeAllCouponCategory()
+        {
+            //try
+            //{
+            //    CouponCategoryData ccd = new CouponCategoryData();
+            //    ccd = appDbContext.CouponCategoryDetails.Sele
+            //}
+            //catch (System.Exception)
+            //{
+
+            //    throw;
+            //}
+            return null;
+        }
+    }
+}
